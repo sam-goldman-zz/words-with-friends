730eee7c16d0386e6f435d88e92246b4af43bb59
Sam Goldman <samgoldman@Sams-MacBook-Pro-4.local>
2021-08-30 22:50:33 -0700

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// Game: Contains the entirety of the project
+// 	Board: Renders a 15x15 grid of Squares
+// 		Square: Renders a single button
+// 	PlayerTiles: Renders a button for each of the user’s tiles
+
+function Square(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <input
+      type="button"
+      value={props.value}
+      // onClick={props.onClick}
+      className="square"/>
+  )
 }
 
-export default App;
+class Board extends React.Component {
+  renderSquare(i) {
+    return (
+      <Square
+        key={i}
+        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}/>
+    )
+  }
+
+  render() {
+    let boardRows = [];
+    let i = 0;
+    for (let r = 0; r < 15; r++) {
+      let squareElements = [];
+      for (let c = 0; c < 15; c++) {
+        squareElements.push(this.renderSquare(i));
+        i++;
+      }
+      boardRows.push(
+        <div key={i} className="board-row">
+          {squareElements}
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        {boardRows}
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    const emptyBoard = new Array(15);
+    for (var i = 0; i < emptyBoard.length; i++) {
+      emptyBoard[i] = new Array(15).fill('');
+    }
+
+    // const LETTERS = ['B', 'D', 'E', 'F', 'Y', 'A', 'R'];
+
+    return (
+      <Board 
+        squares={emptyBoard}
+      />
+    )
+  }
+}
+
+// const LETTERS = {
+//   'A': {
+//     'value': 1,
+//     'count': 9
+//   },
+//   'B': {
+//     'value': 3,
+//     'count': 2
+//   }
+// }
+
+export default Game;
